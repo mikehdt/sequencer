1f2e7b828e7ce386bc99d7ba72bedd5cf90f3724
Mike Hopkins <mike@dthought.net>
2018-03-17 18:25:16 +1100

    Tidy some Todos

--- src/Mustang/Mustang.js
+++ src/Mustang/Mustang.js
@@ -18,13 +18,10 @@ export const EFFECTS = 'effects';
 // TODO / Notes:
 // * Add base effect type that custom effects can extend?
 // * Add a way to preload assets
-// * Sort out the helpers
 // * Fragment shaders?
-// * Investigate Store challenges and listeners and such (Proxy?)
+// * Investigate Store challenges and listeners (Proxy?)
 // * Modifier curves that change the timeline pass-through values?
-// * Some way to request current audio slice for effects to use as vis input?
 // * Force update of Store key notifier?
-// * on- event hooks for Effects (onResize)
 
 function Mustang() {
   createStore();
